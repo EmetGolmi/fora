82b3a120569e60a25d47173482d2d32a311c0745
Noah Goldman <goelsewheretravels@gmail.com>
2026-05-18 14:50:57 -0400

    votemay19 — restored, dynamic polling place from session

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,5 +1,7 @@
 class VoteController < ApplicationController
   def may19
+    @polling_place_name    = session[:polling_place_name]    || "Enter your address on the dashboard"
+    @polling_place_address = session[:polling_place_address] || "to find your polling place"
     render 'vote/may19'
   end
 end
